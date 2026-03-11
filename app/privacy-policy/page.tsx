5086b91f76ac0e3c1ffea8d4656d3d2c412d460c
Michael <michaelkiggen2@gmail.com>
2026-03-11 23:14:12 +0200

    Fixed privacy policy html location

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -4,7 +4,11 @@ import fs from 'fs';
 import path from 'path';
 
 export default function PrivacyPolicyPage() {
-  const filePath = path.join(process.cwd(), 'public', 'privacy-policy.html');
+  const filePath = path.join(
+    process.cwd(),
+    'public/html',
+    'privacy-policy.html',
+  );
   const html = fs.readFileSync(filePath, 'utf8');
 
   return <PrivacyPolicy html={html} />;

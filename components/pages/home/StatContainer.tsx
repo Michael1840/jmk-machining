3207279ad490fafbec424c79777177ce6f4371db
Michael <michaelkiggen2@gmail.com>
2026-06-04 21:23:45 +0200

    Removed stat container

--- components/pages/home/StatContainer.tsx
+++ components/pages/home/StatContainer.tsx
@@ -4,7 +4,6 @@ import StatItem from './StatItem';
 export default function StatContainer() {
   return (
     <Section className="flex flex-col md:flex-row bg-app-container gap-x-4 gap-y-20 justify-around border-y border-border-secondary">
-      <StatItem stat="200+" statName="PROJECTS COMPLETED" />
       <StatItem stat="10+" statName="YEARS EXPERIENCE" />
       <StatItem stat="24/7" statName="EMERGENCY SERVICE" />
     </Section>
